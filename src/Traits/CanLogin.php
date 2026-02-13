cee83d4c284e9d0bf1dbb52d3fbc72dc3b62e3aa
Семён Хорев <stayuglyq@Semens-MacBook-Pro.local>
2026-02-13 14:09:14 +0100

    Implement CanLogin trait

---
+++ src/Traits/CanLogin.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types= 1);
+
+namespace App\Traits;
+
+/**
+ * 
+ * This trait provides login/logout logic.
+ * 
+ */
+
+
+ trait CanLogin
+ {
+    /**
+     * Shows whether user is currently logged in.
+     */
+    protected bool $isLoggedIn = false;
+
+    /**
+     * Check LoggedIn state.
+     * @return bool
+     */
+    public function isLoggedIn(): bool{
+        return $this->isLoggedIn;
+    }
+
+    /**
+     * Attempt to login user with easy validation.
+     * @param string $email
+     * @param string $password
+     * @return bool
+     */
+    public function login(string $email, string $password): bool{
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false ||
+        strlen($password) <= 8) {
+            return false;
+        }
+        else{
+            $this->isLoggedIn = true;
+            return true;
+        }
+    }
+
+ }
